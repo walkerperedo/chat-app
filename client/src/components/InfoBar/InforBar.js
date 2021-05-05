cbb7ebeb76067ca1e1a442ae43330721bbca6471
Walker Peredo <walkerperedo@gmail.com>
2021-05-05 15:19:06 -0400

    Chat aplication advanced

---
+++ client/src/components/InfoBar/InforBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./InfoBar.css";
+
+const InfoBar = () => {
+  return (
+    <div className="infoBar">
+      <div className='leftInnerContainer'></div>
+      <div className='rightInnerContainer'></div>
+      
+    </div>
+  );
+};
+
+export default InfoBar;
